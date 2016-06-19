110c7e955abb29ca175753f78210ea2f959b3818
yurii-kurylenko <yuriy.kurilenko7@gmail.com>
2016-06-20 00:48:29 +0300

    Add atm lock support

--- lib/atm_network/atm/server.ex
+++ lib/atm_network/atm/server.ex
@@ -8,7 +8,7 @@ defmodule AtmNetwork.Atm.Server do
 
   def init(state_pid) do
     payload = AtmNetwork.Atm.StateServer.get_payload(state_pid)
-    {:ok, {payload, state_pid}}
+    {:ok, {payload, state_pid, false}}
   end
 
   def current_payload(atm) do
@@ -27,6 +27,10 @@ defmodule AtmNetwork.Atm.Server do
     GenServer.cast(atm, {:clean})
   end
 
+  def toggle_lock(atm) do
+    GenServer.call(atm, {:toggle_lock})
+  end
+
   defp via_tuple(name) do
     {:via, :gproc, {:n, :l, {:atm, name}}}
   end
@@ -40,7 +44,7 @@ defmodule AtmNetwork.Atm.Server do
   end
   ###
 
-  def handle_call({:current_payload}, _, {payload, _} = state) do
+  def handle_call({:current_payload}, _, {payload, _, _} = state) do
     {
       :reply,
       payload,
@@ -48,35 +52,57 @@ defmodule AtmNetwork.Atm.Server do
     }
   end
 
-  def handle_call({:exchange, amount}, _, {payload, state_pid} = state) do
+  def handle_call({:exchange, amount}, _, {payload, state_pid, locked} = state) when locked == false do
     case Payload.exchange(payload, amount) do
       {:ok, left, exchanged} ->
         AtmNetwork.Atm.StateServer.save_payload(state_pid, left)
-        {:reply, {exchanged, left}, {left, state_pid}}
+        {:reply, {:ok, {exchanged, left} }, {left, state_pid, locked}}
       {:error, reason} ->
-        {:reply, reason, state}
+        {:reply, {:error, reason}, state}
     end
   end
 
-  def handle_cast({:put, cache}, {payload, state_pid}) do
+  def handle_call({:exchange, _}, _, {_, _, locked} = state) when locked == true do
+    {:reply, {:error, "Atm is locked"}, state}
+  end
+
+  def handle_cast({:put, cache}, {payload, state_pid, locked}) when locked == false do
     new_payload = Payload.add(payload, cache)
     AtmNetwork.Atm.StateServer.save_payload(state_pid, new_payload)
     {
       :noreply,
-      {new_payload, state_pid}
+      {new_payload, state_pid, locked}
     }
   end
 
-  def handle_cast({:clean}, {_, state_pid}) do
+  def handle_cast({:put, _}, {_, _, locked} = state) when locked == true do
+    {
+      :noreply,
+      state
+    }
+  end
+
+  def handle_cast({:clean}, {_, state_pid, locked}) when locked == false do
     AtmNetwork.Atm.StateServer.save_payload(state_pid, Payload.new)
     {
       :noreply,
-      Payload.new
+      {Payload.new, state_pid, locked}
+    }
+  end
+
+  def handle_cast({:clean}, {_, _, locked} = state) when locked == true do
+    {
+      :noreply,
+      state
     }
   end
 
   def handle_cast({:shutdown}, state) do
     {:stop, :normal, state}
+  end
+
+  def handle_call({:toggle_lock}, _, {payload, state_pid, locked}) do
+    {:reply, {:ok, !locked}, {payload, state_pid, !locked}}
   end
 
   def handle_info(:stop, state), do: {:stop, :normal, state}
